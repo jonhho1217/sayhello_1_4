f9d37cf04c7ed8a4e47bb8c4fd7d06133a906504
jonhho1217 <32205538+jonhho1217@users.noreply.github.com>
2019-01-18 23:34:44 -0800

    Update main.dart
    save

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:sayhello/container/container.dart';
 import 'package:sayhello/constants.dart';
-
-import 'package:sayhello/screens/start_page.dart';
+import 'package:sayhello/screens/login_page.dart';
 import 'package:sayhello/screens/register_page.dart';
 import 'package:sayhello/themes.dart';
 
-void main() => runApp(new StateContainer(child: new SayHelloApp()));
+Future<void> main() async {
+  String googlappid = '1:1078068933478:android:668ae3e8dfce9ad5';
+  if (Platform.isIOS) {
+    googlappid = '1:1078068933478:ios:80ddcc18ff9fd6b7';
+  }
+
+  final FirebaseApp app = await FirebaseApp.configure(
+    name: 'SayHello',
+    options: FirebaseOptions(
+      googleAppID: googlappid,
+      apiKey: 'AIzaSyC0ALz3dlDvlHw5WJACMRSc9g-H0D7e5Oc',
+      projectID: 'sayhello-bed85',
+    ),
+  );
+  final Firestore firestore = Firestore(app: app);
+  await firestore.settings(timestampsInSnapshotsEnabled: true);
+
+  runApp(new StateContainer(child: new SayHelloApp(firestore: firestore)));
+}
 
 class SayHelloApp extends StatelessWidget {
 
+  SayHelloApp({this.firestore});
+  final Firestore firestore;
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
         title: Constants.APP_NAME,
         theme: Themes.getTheme(context),
